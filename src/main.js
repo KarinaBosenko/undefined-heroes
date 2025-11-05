ceb5c6737e6a8763a919bfd3fccb50f48cf4c2a9
Karina Bosenko <karina.bosenko.10.10@gmail.com>
2025-11-05 17:51:37 +0200

    initialize project and add base configuration

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header';
+import './js/furniture-list';
+import './js/popular-products';
+import './js/faq';
+import './js/feedback';
+import './js/modal-details';
+import './js/modal-order';

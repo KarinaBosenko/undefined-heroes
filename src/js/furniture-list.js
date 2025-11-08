67e3744f90e193f552a60fd4c6eb7191afaa464a
Kira Yukhno <kira.yukhno123@gmail.com>
2025-11-08 20:04:10 +0200

    do section out furniture

---
+++ src/js/furniture-list.js
@@ -0,0 +1,249 @@
+import {
+    fetchCategories, 
+    fetchDefaultFurniture,
+    fetchFurnitureByCategory,
+} from "./furniture-api.js";
+import {
+    openDetailsModal,
+} from "./modal-details.js";
+
+const furnitureCategories = document.querySelector("ul.furniture-categories");
+const furnitureGallery = document.querySelector("ul.furniture-gallery");
+const loadMoreBtn = document.querySelector("button.furniture-load-more-btn");
+const loader = document.querySelector("span.loader");
+
+//saving order from design
+const categories = [
+    {
+        _id: "all",
+        name: "Всі товари",
+        image: "./img/furniture_list_section/all1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7b8",
+        name: "М'які меблі",
+        image: "./img/furniture_list_section/sofa1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7b9",
+        name: "Шафи та системи зберігання",
+        image: "./img/furniture_list_section/wardrobe1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7ba",
+        name: "Ліжка та матраци",
+        image: "./img/furniture_list_section/bed1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7bb",
+        name: "Столи",
+        image: "./img/furniture_list_section/table1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7bc",
+        name: "Стільці та табурети",
+        image: "./img/furniture_list_section/chairs1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7bd",
+        name: "Кухні",
+        image: "./img/furniture_list_section/kitchen1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7be",
+        name: "Меблі для дитячої",
+        image: "./img/furniture_list_section/kid1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7bf",
+        name: "Меблі для офісу",
+        image: "./img/furniture_list_section/office1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7c0",
+        name: "Меблі для передпокою",
+        image: "./img/furniture_list_section/entryway1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7c1",
+        name: "Меблі для ванної кімнати",
+        image: "./img/furniture_list_section/bathroom1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7c2",
+        name: "Садові та вуличні меблі",
+        image: "./img/furniture_list_section/garden1x-min.jpg",
+    },
+    {
+        _id: "66504a50a1b2c3d4e5f6a7c3",
+        name: "Декор та аксесуари",
+        image: "./img/furniture_list_section/decor1x-min.jpg",
+    },
+];
+
+//adding a list of categories
+export const categoriesList = categories => {
+    furnitureCategories.insertAdjacentHTML("beforeend", categories
+        .map(({
+            _id,
+            name,
+            image,
+        }) =>
+            `
+            <li class="category-item">
+                <a class="category-link" data-category="${_id}">
+                    <div class="category-img-wrapper">
+                        <img class="category-img" src="${image}" srcset="${image} 1x, ${image.replace('1x', '2x')} 2x" alt="${name}"/>
+                        <div class="category-overlay">${name}</div> 
+                    </div>
+                </a>
+            </li>
+            `
+        ).join("")
+    )
+};
+
+//creating gallery
+export const createGallery = images => {
+    furnitureGallery.insertAdjacentHTML("beforeend", images
+        .map(
+            ({
+                _id,
+                name,
+                color,
+                price,
+                images
+            }) => {
+                const colorPick = color.map(option => `<span class="color-pick" style="background-color:${option};"></span>`).join("");
+                return `
+                <li class="gallery-item" data-id="${_id}">
+                    <img src="${images[0]}" alt="${name}" class="furniture-img"/>
+                    <div class="gallery-item-dscr">
+                        <p class="furniture-title">${name}</p>
+                        <div class="color" data-id="${color}">${colorPick}</div>
+                        <p class="price">${price} грн</p>                    
+                    </div>
+                    <button type="button" class="gallery-item-info-btn">Детальніше</button>
+                </li>
+                `;
+            }).join("")
+    );
+};
+
+export const clearGallery = () => {
+    furnitureGallery.innerHTML = "";
+};
+
+export const showLoader = () => {
+    loader.classList.remove("hidden");
+};
+
+export const hideLoader = () => {
+    loader.classList.add("hidden");
+};
+
+//setting current category
+export const setActiveCategory = categoryId => {
+    furnitureCategories.querySelectorAll(".category-link").forEach(link => {
+        const isActive = link.getAttribute("data-category") === categoryId;
+        link.classList.toggle("active", isActive);
+
+        const activeCategoryOverlay = link.querySelector(".category-overlay");
+        if (activeCategoryOverlay) {
+            activeCategoryOverlay.classList.remove("is-active");
+            if (isActive) {
+            activeCategoryOverlay.classList.add("is-active");            
+            }
+        }  
+    });
+};
+
+let currentPage = 1;
+let currentCategory = "all";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        categoriesList(categories);
+        showLoader();
+        const data = await fetchDefaultFurniture(currentPage);
+
+        if (data.furnitures.length < 8) {
+            loadMoreBtn.disabled = true;
+        }
+        else {
+            loadMoreBtn.disabled = false;
+        }
+        createGallery(data.furnitures);
+        hideLoader();
+        setActiveCategory("all");
+    } catch (error) {
+        console.error("Помилка при завантаженні меблів:", error);
+        hideLoader();
+    }
+});
+
+//items by category
+furnitureCategories.addEventListener("click", async event => {
+    const link = event.target.closest(".category-link");
+    if (!link) return;
+
+    const categoryId = link.dataset.category;
+    setActiveCategory(categoryId);
+    clearGallery();
+    showLoader();
+
+    try {
+        currentPage = 1;
+        currentCategory = categoryId;
+        let data;
+        if (categoryId === "all") {
+        data = await fetchDefaultFurniture(currentPage);
+        } else {
+        data = await fetchFurnitureByCategory(categoryId, currentPage);
+        }
+
+        if (data.furnitures.length < 8) {
+            loadMoreBtn.disabled = true;
+        }
+        else {
+            loadMoreBtn.disabled = false;
+        }
+
+        createGallery(data.furnitures);
+    } catch (error) {
+        console.error("Помилка при завантаженні меблів по категорії:", error);
+    } finally {
+        hideLoader();
+    }
+});
+
+//additional items
+loadMoreBtn.addEventListener("click", async () => {
+    currentPage += 1;
+    showLoader();
+    try {
+        let data; 
+        if (currentCategory === "all") {
+        data = await fetchDefaultFurniture(currentPage);
+        } else {
+        data = await fetchFurnitureByCategory(currentCategory, currentPage);
+        }
+
+        if (data.furnitures.length < 8) {
+            loadMoreBtn.disabled = true;
+        }
+        createGallery(data.furnitures);
+    } catch (error) {
+        console.error("Помилка при завантаженні меблів:", error);
+    } finally {
+        hideLoader();
+    }
+});
+
+//opening modal
+furnitureGallery.addEventListener("click", event => {
+    if (event.target.classList.contains("gallery-item-info-btn")) {
+        const itemId = event.target.closest(".gallery-item").dataset.id;
+        openDetailsModal(itemId);
+    }
+});

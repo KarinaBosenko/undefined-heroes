69a7b9b64724bb6ddb16a5af03e7ba5baafc944b
Marina Yastrebova <yasstreb@gmail.com>
2025-11-10 22:37:45 +0200

    add star rating as svg

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,6 +1,7 @@
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'css-star-rating/css/star-rating.css';
 
 import { fetchFeedbacks } from './furniture-api';
 import Swiper from 'swiper';
@@ -18,23 +19,51 @@ function roundRating(rating) {
   return Math.round(rating * 2) / 2;
 }
 
+function generateStars(rating) {
+  const fullStars = Math.floor(rating);
+  const hasHalf = rating % 1 !== 0;
+  const totalStars = 5;
+
+  let starsHtml = '';
+
+  for (let i = 0; i < totalStars; i++) {
+    let iconId = 'star-empty';
+
+    if (i < fullStars) {
+      iconId = 'star-filled';
+    } else if (i === fullStars && hasHalf) {
+      iconId = 'star-half';
+    }
+
+    starsHtml += `
+      <div class="star">
+        <svg width="24" height="24" viewBox="0 0 34 32">
+          <use href="./img/icons.svg#${iconId}"></use>
+        </svg>
+      </div>
+    `;
+  }
+
+  return starsHtml;
+}
+
 function renderFeedbacks(feedbacks) {
   const listElement = document.getElementById('feedback-list');
   listElement.innerHTML = '';
 
   feedbacks.forEach(feedback => {
     const roundedRating = roundRating(feedback.rate);
-    const ratingValue = roundedRating.toFixed(1);
-
-    const ratingPercent = (roundedRating / 5) * 100;
+    const hasHalf = roundedRating % 1 !== 0;
+    const valueClass = `value-${Math.floor(roundedRating)}`;
+    const halfClass = hasHalf ? 'half' : '';
 
     const cardHtml = `
         <li class="feedback-card swiper-slide">
-            <div 
-                class="star-rating" 
-                data-rating="${ratingValue}"
-                style="--star-rating: ${ratingPercent}%" 
-            ></div> 
+            <div class="rating ${valueClass} ${halfClass} star-svg medium color-default">
+              <div class="star-container">
+                ${generateStars(roundedRating)}
+              </div>
+            </div>
             <p class="feedback-text">"${feedback.descr}"</p>
             <p class="feedback-author">${feedback.name}</p>
         </li>
@@ -102,7 +131,9 @@ async function initFeedbackBlock() {
 
     renderFeedbacks(feedbacksArray);
     initSwiper();
-  } catch (error) {}
+  } catch (error) {
+    console.error('Помилка завантаження відгуків:', error);
+  }
 }
 
 initFeedbackBlock();

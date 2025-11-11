7896130c65a8ddf9e7edf9013b49cc33643c0910
Karina Bosenko <karina.bosenko.10.10@gmail.com>
2025-11-11 07:47:01 +0200

    Update img

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: './',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
